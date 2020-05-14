592ed9ad384b5cf21862e4fbb85f4c16df6190ae
Krish Maniar <56781484+krinetic1234@users.noreply.github.com>
2020-05-13 23:05:25 -0700

    Add files via upload

---
+++ build/precache-manifest.ae272dd13e75ba51c4a991f6d18a4c6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e4577f6609c942034014357cd22ae9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38826deb8bf6b644693e",
+    "url": "/static/css/main.7c8f2f72.chunk.css"
+  },
+  {
+    "revision": "a2d520d69ee19d169820",
+    "url": "/static/js/2.51591260.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.51591260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38826deb8bf6b644693e",
+    "url": "/static/js/main.65e797cf.chunk.js"
+  },
+  {
+    "revision": "69870f6509be034a7b18",
+    "url": "/static/js/runtime-main.2a68ab28.js"
+  }
+]);
